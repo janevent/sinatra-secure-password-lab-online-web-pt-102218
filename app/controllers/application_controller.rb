f15b292c21e721a044b97ca7857531dc69cc3f8e
Jane Venter <janeanneventer@gmail.com>
2019-03-02 22:09:50 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     if !params[:username].empty? && !params[:password].empty?
-      user = User.new(:username => params[:username], :password =>params[:password])
+      user = User.new(:username => params[:username], :password => params[:password])
       user.save
       
       redirect '/login'
